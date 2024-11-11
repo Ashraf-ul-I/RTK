8faf43be90044cb5a4de2d8c985ae13464fdd49f
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-11-11 22:15:13 +0600

    Routing: use Link to navigate through the item or component

--- videoWebsite/src/components/list/RelatedVideoListItem.jsx
+++ videoWebsite/src/components/list/RelatedVideoListItem.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom"
 
 const RelatedVideoListItem = () => {
   return (
@@ -6,13 +6,13 @@ const RelatedVideoListItem = () => {
         <div
             className="relative w-[168px] h-[94px] flex-none duration-300 hover:scale-[1.03]"
         >
-            <a href="video.html">
+            <Link to="/video/1">
                 <img
                     src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
                     className="object-cover"
                     alt="Some video title"
                 />
-            </a>
+            </Link>
             <p
                 className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py"
             >
@@ -21,19 +21,19 @@ const RelatedVideoListItem = () => {
         </div>
 
         <div className="flex flex-col w-full">
-            <a href="#">
+            <Link to="/video/1">
                 <p
                     className="text-slate-900 text-sm font-semibold"
                 >
                     Some video title
                 </p>
-            </a>
-            <a
+            </Link>
+            <Link
                 className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-                href="#"
+                to="/video/1"
             >
                 Learn with Sumit
-            </a>
+            </Link>
             <p className="text-gray-400 text-xs mt-1">
                 100K views . 23 Oct 2022
             </p>
